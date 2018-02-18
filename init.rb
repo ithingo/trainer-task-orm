1cfe5dc25373aee7b2e8f03120aa00d6ffd5b9f0
Vlad Borodaev <nikoskefali@gmail.com>
2018-02-18 17:32:38 +0300

    Deleted test strings showing actions

--- init.rb
+++ init.rb
@@ -1,30 +1,12 @@
 require 'pg'
 
-# Dir[File.dirname(__FILE__) + '/src/modules/*.rb'].each { |file| require file }
-# Dir[File.dirname(__FILE__) + '/src/*.rb'].each { |file| require file }
+# For Mac
+Dir[File.dirname(__FILE__) + '/src/modules/*.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + '/src/*.rb'].each { |file| require file }
 
-require_relative 'src/modules/db_connection'
-require_relative 'src/modules/own_exceptions'
-require_relative 'src/modules/type_operations'
-require_relative 'src/modules/work_with_table'
-require_relative 'src/basic_orm'
-
-test_1 = BasicORM.new do |app|
-  app.create_table do |table|
-    table.add_column :name, table.string, 'not null primary key'
-    table.add_column :age, table.integer, 'not null'
-    table.add_column :address, table.text
-  end
-end
-
-test_1.create(name: "Vladimir Putin", age: 64, address: "Moscow")
-test_1.create(name: "Donald J. Trump", age: 71, address: "Washington D.C.")
-
-test_1.save
-
-test_1.delete(where: 'name like Donald%')
-test_1.delete(where: 'age > 34')
-
-test_1.update(where: 'age = 64', change: 'age = 65')
-
-test_1.clear_all!
+# For linux
+# require_relative 'src/modules/db_connection'
+# require_relative 'src/modules/own_exceptions'
+# require_relative 'src/modules/type_operations'
+# require_relative 'src/modules/work_with_table'
+# require_relative 'src/basic_orm'
